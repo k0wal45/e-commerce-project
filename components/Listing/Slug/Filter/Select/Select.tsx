397475c23c0137fe3d514b39c9bb1ab62e72c14a
k0wal45 <d.kowal.com@gmail.com>
2025-01-27 22:58:43 +0100

    save for tommorow

--- components/Listing/Slug/Filter/Select/Select.tsx
+++ components/Listing/Slug/Filter/Select/Select.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import classes from "./select.module.scss";
+import { motion } from "framer-motion";
 
-const Select = () => {
-  return <div></div>;
+const Select = ({
+  selected,
+  setSelected,
+  values,
+}: {
+  selected: string;
+  setSelected: any;
+  values: string[];
+}) => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Select;
