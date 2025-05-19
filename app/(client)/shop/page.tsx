db0beb300eb1cb78792df6d8e31ed0b2a4f2248d
k0wal45 <d.kowal.com@gmail.com>
2025-05-19 16:32:26 +0200

    screenshots to documentation added

--- app/(client)/shop/page.tsx
+++ app/(client)/shop/page.tsx
@@ -1,6 +1,6 @@
 import UniversalHero from "@/components/Hero/Universal/UniversalHero";
 import DisplayListings from "@/components/Listing/Shop/DisplayListings";
-import Filter from "@/components/Listing/Shop/Filter/Filter";
+// import Filter from "@/components/Listing/Shop/Filter/Filter";
 import { Suspense } from "react";
 
 const Page = () => {
@@ -11,7 +11,7 @@ const Page = () => {
         image="https://images.unsplash.com/photo-1495368427887-e88d33a44e0f?q=80&w=2053&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         position="center"
       />
-      <Filter />
+      {/* <Filter /> */}
       <Suspense fallback={<div>Loading...</div>}>
         <DisplayListings />
       </Suspense>
